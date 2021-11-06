ded305483bc0874d0506b4b9af09743cc5f738bb
Dhevanthareza <dhevanmuhamad@gmail.com>
2021-11-07 00:55:05 +0700

    beneri input pembayaran

--- app/src/main/java/com/kuliahdhevan/pasardesasambungrejo/PurchaseDetailActivity.java
+++ app/src/main/java/com/kuliahdhevan/pasardesasambungrejo/PurchaseDetailActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class PurchaseDetailActivity extends AppCompatActivity {
@@ -17,5 +21,35 @@ public class PurchaseDetailActivity extends AppCompatActivity {
         String purchaseAmount = intent.getStringExtra("purchaseAmount");
         TextView purchaseAmountTv = findViewById(R.id.purchaseAmount);
         purchaseAmountTv.setText(purchaseAmount);
+        EditText edtPaymentAmount = findViewById(R.id.editTextNumber);
+        edtPaymentAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                changePaymentAmount();
+            }
+        });
     }
-}
+
+    public void changePaymentAmount() {
+        Intent intent = getIntent();
+        Integer purchaseAmount = Integer.parseInt(intent.getStringExtra("purchaseAmount")) ;
+        EditText edtPaymentAmount = findViewById(R.id.editTextNumber);
+        TextView returnAmountText = findViewById(R.id.textView18);
+        if(edtPaymentAmount.getText().toString().equals("") || edtPaymentAmount.getText().toString().equals("0")) {
+            return;
+        }
+        Integer paymentAmount = Integer.parseInt(edtPaymentAmount.getText().toString());
+        Integer returnAmount = paymentAmount - purchaseAmount;
+        returnAmountText.setText(returnAmount.toString());
+    }
+};
